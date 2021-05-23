fixed sign problem

URBAN/urban_main.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import urban_utils as utils
+np.seterr(all="ignore")
 
 car = AVISEngine.car()
 car.connect("127.0.0.1", 25001)
@@ -85,7 +86,7 @@ while(True):
     
                 if mean_pix < 128:
                     if side_pix > 128:
-                        utils.go_back(car,4)
+                        utils.go_back(car,4.5)
                         utils.turn_the_car(car,-100,10)
                     else:
                         utils.turn_the_car(car,-80,8)
@@ -93,7 +94,7 @@ while(True):
                     if side_pix < 128:
                         utils.go_back(car,8)
                     else:
-                        utils.go_back(car,4)
+                        utils.go_back(car,4.5)
                     utils.turn_the_car(car,100,10)
                 
             elif sign_state == 'left':

URBAN/urban_utils.py
@@ -104,7 +104,8 @@ def detect_side(side_mask):
 
 def detect_sign(frame, hsv_frame):
     types = ['left', 'straight', 'right']
-    mask = cv2.inRange(hsv_frame, np.array([100,170,90]), np.array([160,220,220]))
+    mask = cv2.inRange(hsv_frame, np.array([100,160,90]), np.array([160,220,220]))
+    mask[:30,:]=0
     try:
         points, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         sorted_points = sorted(points, key=len)